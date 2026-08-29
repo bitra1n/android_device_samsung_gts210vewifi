0b0f7f869d7239678161f77568e710fc2d898d33
bitra1n <275021827+bitra1n@users.noreply.github.com>
2026-08-29 13:42:48 +0200

    gts210vewifi: config: declare NO_CAMERA
    Change-Id: Ibd24c5bd524ec201eab49c49de47f5037fb6b908

--- lineage_gts210vewifi.mk
+++ lineage_gts210vewifi.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# No camera support, since it's half-broken anyway.
+PRODUCT_NO_CAMERA := true
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
